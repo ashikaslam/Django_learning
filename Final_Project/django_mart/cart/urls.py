8e786db91c18d02109476d011aa5a56f6884fbfd
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-03-07 11:27:30 +0600

    somethin new

--- Final_Project/django_mart/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-   # path('make_payment/', views.make_payment, name='make_payment'),
-    path('', views.cart, name='cart'),
-    path('<int:product_id>/', views.add_to_cart, name='add_cart'),
-    path('decrese/<int:product_id>/', views.remove_form_cart, name='remove_form_cart'),
-    path('remove/<int:product_id>/', views.removeall_form_cart, name='remove_item_form_cart'),
-]
