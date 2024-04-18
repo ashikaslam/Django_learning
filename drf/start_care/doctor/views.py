400f5fb11b880ec197081442e8337387c90f7f11
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-04-18 10:37:33 +0600

    done

--- drf/start_care/doctor/views.py
+++ drf/start_care/doctor/views.py
@@ -1,13 +1,17 @@
 from rest_framework import viewsets
 from . import models
 from. import serializers
+from. import Paginations
 
+from rest_framework.permissions import BasePermission
+from rest_framework import filters
 class DoctorViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list` and `retrieve` actions.
     """
     queryset = models.Doctor.objects.all()
     serializer_class = serializers.DoctorSerializers
+    pagination_class = Paginations.StandardResultsSetPagination
 
 class DesignationViewSet(viewsets.ModelViewSet):
     """
@@ -23,12 +27,22 @@ class SpecializationViewSet(viewsets.ModelViewSet):
     queryset = models.Specialization.objects.all()
     serializer_class = serializers.SpecializationSerializers
 
+
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, query_set, view):
+        doctor_id = request.query_params.get("doctor_id")
+        if doctor_id:
+            return query_set.filter(doctor = doctor_id)
+        return query_set
+
+
 class AvailableTimeViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list` and `retrieve` actions.
     """
     queryset = models.AvailableTime.objects.all()
     serializer_class = serializers.AvailableTimeSerializers
+    filter_backends = [AvailableTimeForSpecificDoctor]
 
 class ReviewViewSet(viewsets.ModelViewSet):
     """
@@ -36,4 +50,5 @@ class ReviewViewSet(viewsets.ModelViewSet):
     """
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializers
+  
 
