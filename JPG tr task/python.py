90ee265e070824bae70797ebc18d179bae4ed788
Ashik Aslam <158561158+ashikaslam@users.noreply.github.com>
2024-05-15 20:06:54 +0600

    123

---
+++ JPG tr task/python.py
@@ -0,0 +1,135 @@
+
+
+
+1:
+
+print("hello world",end="")
+print("im aslam")
+
+what will be the out put?
+
+a) hello world im aslam
+b) hello worldim aslam
+b) hello world
+   im aslam
+
+ans is b
+
+2:
+
+name = "aslma"
+name[0]='b'
+print(name)
+
+what will be the output?
+
+a)bslam
+b)aslam
+c)error
+
+ans is c
+
+
+3:
+
+ar = [1,2,3]
+def my_fun(a):
+   a[0]=10
+
+my_fun(ar)
+
+the change inside the the function does not effect the real arr because will just pass a copy list of ar.
+   
+a) true 
+b ) False
+
+ans is b
+
+
+4:
+
+map = {'name':"aslam","roll":57963,"dip":"cse"}
+if the len of the map is N then what will be the timecomplexity of accessing data form the map?
+
+a) O(logN)
+b) ONlogN)
+c O(N)
+
+ans is is c
+
+5:
+
+my_tuple=(1,2,3,['monir'])
+my_tuple[-1]="ashik"
+
+a) we will get error
+b) we will not get error?
+
+a is True
+
+
+6:
+
+my_tuple=(1,2,3,['monir'])
+my_tuple[-1][-1]="n"
+
+a) we will get error
+b) we will not get error?
+
+a is True
+
+
+
+7: 
+set = {}
+print(type(set))
+
+what will be the output ?
+a)set
+b)list
+c)dict
+
+the ans is  c
+
+8:
+my_set = {1,2,3,4,5}
+your_set={1,2,5}
+print(my_set-your_set)
+
+what will be the out put?
+
+a) {}
+b) {3, 4}
+c) error
+d) none of them
+
+ans is b
+
+9:
+our_ar = [1,2,3,5,6]
+print(our_ar[0::2])
+
+what will be the out put ?
+
+a)[1]
+b=[1, 3, 6]
+c=[1,2,3]
+the ans is b
+
+10:
+
+b = 1 and 0 or 1 and 'x'=='x'
+print(type(b))
+
+a) out will be bool 
+b error 
+
+ans is a
+
+
+
+
+
+
+
+
